1e1d8d518c9a5a785bf57fd5f08f3d21ed645845
TAN JIN WEI <jwtan@rebel-owl.com>
2019-11-04 12:54:35 +0800

    finished tutorial

--- src/client/modules/my/app/app.js
+++ src/client/modules/my/app/app.js
@@ -1,30 +1,30 @@
 import { LightningElement, track } from 'lwc';
 
 export default class App extends LightningElement {
-  @track sessionId;
-  @track state;
+    @track sessionId;
+    @track state;
 
-  constructor() {
-    super();
-    this.state = 'list';
-    window.history.replaceState('list', null, '');
-    window.onpopstate = event => {
-      if (event.state) {
-        this.state = event.state;
-      }
-    };
-  }
+    constructor() {
+        super();
+        this.state = 'list';
+        window.history.replaceState('list', null, '');
+        window.onpopstate = event => {
+            if (event.state) {
+                this.state = event.state;
+            }
+        };
+    }
 
-  handleNavigate(event) {
-    this.sessionId = event.detail;
-    this.state = 'details';
-    window.history.pushState('details', null);
-  }
+    handleNavigate(event) {
+        this.sessionId = event.detail;
+        this.state = 'details';
+        window.history.pushState('details', null);
+    }
 
-  get isStateList() {
-    return this.state === 'list';
-  }
-  get isStateDetails() {
-    return this.state === 'details';
-  }
+    get isStateList() {
+        return this.state === 'list';
+    }
+    get isStateDetails() {
+        return this.state === 'details';
+    }
 }
